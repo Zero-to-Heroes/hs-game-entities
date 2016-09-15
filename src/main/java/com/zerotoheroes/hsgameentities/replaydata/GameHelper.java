5005edd689c6864ed6305a72d741922c9e001a84
sebastientromp <sebastien.tromp@gmail.com>
2016-09-15 16:23:04 +0200

    add helper methods

--- src/main/java/com/zerotoheroes/hsgameentities/replaydata/GameHelper.java
+++ src/main/java/com/zerotoheroes/hsgameentities/replaydata/GameHelper.java
@@ -6,10 +6,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.zerotoheroes.hsgameentities.enums.CardType;
+import com.zerotoheroes.hsgameentities.enums.GameTag;
 import com.zerotoheroes.hsgameentities.replaydata.entities.BaseEntity;
 import com.zerotoheroes.hsgameentities.replaydata.entities.FullEntity;
 import com.zerotoheroes.hsgameentities.replaydata.entities.PlayerEntity;
 import com.zerotoheroes.hsgameentities.replaydata.gameactions.Action;
+import com.zerotoheroes.hsgameentities.replaydata.gameactions.ShowEntity;
+import com.zerotoheroes.hsgameentities.replaydata.gameactions.Tag;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -29,16 +35,19 @@ public class GameHelper {
 	}
 
 	private void buildFullEntityList() {
-		flatData.addAll(game.getData());
+		// flatData.addAll(game.getData());
+		// Keep the chronological ordering
 		for (GameData data : game.getData()) {
+			flatData.add(data);
 			extractData(data);
 		}
 	}
 
 	private void extractData(GameData data) {
 		if (data instanceof Action) {
-			flatData.addAll(((Action) data).getData());
+			// flatData.addAll(((Action) data).getData());
 			for (GameData gameData : ((Action) data).getData()) {
+				flatData.add(gameData);
 				extractData(gameData);
 			}
 		}
@@ -56,7 +65,6 @@ public class GameHelper {
 		return entity;
 	}
 
-	@SuppressWarnings("unchecked")
 	public <T extends GameData> List<T> filterGameData(Class... specificClasses) {
 		return (List<T>) flatData.stream().filter(e -> Arrays.asList(specificClasses).contains(e.getClass()))
 				.collect(Collectors.toList());
@@ -65,6 +73,32 @@ public class GameHelper {
 	public List<PlayerEntity> getPlayers() {
 		return flatData.stream().filter(d -> (d instanceof PlayerEntity)).map(p -> (PlayerEntity) p)
 				.collect(Collectors.toList());
+	}
+
+	public PlayerEntity getMainPlayer() {
+		// Since patch 5.2.0.13619, the first showEntity with a cardID (and that
+		// is not an enchantment, cf tavern brawl conditions) always comes from
+		// the current player
+		List<ShowEntity> showEntities = filterGameData(ShowEntity.class);
+		for (ShowEntity entity : showEntities) {
+			if (StringUtils.isNotEmpty(entity.getCardId())
+					&& getTag(entity.getTags(), GameTag.CARDTYPE) != CardType.ENCHANTMENT.getIntValue()) {
+				int entityId = entity.getEntity();
+				// The full entity
+				BaseEntity fullEntity = getEntity(entityId);
+				// Now check the CONTROLLER tag
+				int controllerId = getTag(fullEntity.getTags(), GameTag.CONTROLLER);
+				for (PlayerEntity player : getPlayers()) {
+					if (getTag(player.getTags(), GameTag.CONTROLLER) == controllerId) { return player; }
+				}
+			}
+		}
+		return null;
+	}
+
+	private int getTag(List<Tag> tags, GameTag tag) {
+		Tag ret = tags.stream().filter(t -> t.getName() == tag.getIntValue()).findFirst().orElse(null);
+		return ret == null ? -1 : ret.getValue();
 	}
 
 }
